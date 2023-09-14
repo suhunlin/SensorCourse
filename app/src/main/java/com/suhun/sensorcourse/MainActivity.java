a22aabdb313e866b899a7bcd967c9f00cedab1ee
suhunlin <lin.suhun@msa.hinet.net>
2023-09-14 17:50:15 +0800

    Add layout and initial TextView

--- app/src/main/java/com/suhun/sensorcourse/MainActivity.java
+++ app/src/main/java/com/suhun/sensorcourse/MainActivity.java
@@ -3,12 +3,22 @@ package com.suhun.sensorcourse;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView v1, v2, v3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView(){
+        v1 = findViewById(R.id.lid_v1);
+        v2 = findViewById(R.id.lid_v2);
+        v3 = findViewById(R.id.lid_v3);
     }
 }
